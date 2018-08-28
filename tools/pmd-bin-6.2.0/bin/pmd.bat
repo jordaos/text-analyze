ecc5f8fc81f393c8b595186bccd84ecba5147aac
Jordão <jordao05@hotmail.com>
2018-08-28 22:02:03 +0000

    remove static analysis

--- tools/pmd-bin-6.2.0/bin/pmd.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set TOPDIR=%~dp0..
-set OPTS=
-set MAIN_CLASS=net.sourceforge.pmd.PMD
-
-java -classpath "%TOPDIR%\lib\*;..\lib\pmd-3.4.jar;..\lib\jaxen-1.1-beta-7.jar" %OPTS% %MAIN_CLASS% %*
